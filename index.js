de6f9a4958d1dcbbadc0094b1319673bf8e719a8
David Drost <>
2021-04-13 16:00:57 +0000

    more fixes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var _ = require('lodash');
-
-console.clear()
+const _ = require('lodash');
 
 const origTopics = [
     [
@@ -26,6 +24,23 @@ const origTopics = [
             "FORM_TOPIC_ID": "23",
             "XS_COURSE_TOPIC_NM": "Communications"
         }
+    ],
+    [
+        {
+            "COURSE_TOPIC_ID": "122",
+            "FORM_TOPIC_ID": "22",
+            "XS_COURSE_TOPIC_NM": "Behaviour Management and Mental Health"
+        },
+        {
+            "COURSE_TOPIC_ID": "195",
+            "FORM_TOPIC_ID": "21",
+            "XS_COURSE_TOPIC_NM": "Back in Form"
+        },
+        {
+            "COURSE_TOPIC_ID": "132",
+            "FORM_TOPIC_ID": "23",
+            "XS_COURSE_TOPIC_NM": "Communications"
+        }
     ]
 ];
 
@@ -35,11 +50,12 @@ origTopics.forEach(topic => {
     topics.push(...topic);
 })
 
+console.clear();
 console.log("\n\ntopics:",topics);
 
-const uniqueTopics = _.uniq(topics, 'COURSE_TOPIC_ID')
+const uniqueTopics = _.uniqWith(topics, _.isEqual)
 console.log("\n\nuniqueTopics:",uniqueTopics);
 
-const sortedTopics = _.sortBy(uniqueTopics, ['XS_COURSE_TOPIC_NM']);
+const sortedTopics = _.sortBy(uniqueTopics, 'XS_COURSE_TOPIC_NM');
 
 console.log("\n\nsortedTopics:",sortedTopics);
