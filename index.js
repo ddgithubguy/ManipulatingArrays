e4f5e6087acf2652053390ddae2b7382d953744d
David Drost <>
2021-04-13 16:18:08 +0000

    refactor

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ const origTopics = [
         {
             "COURSE_TOPIC_ID": "195",
             "FORM_TOPIC_ID": "21",
-            "XS_COURSE_TOPIC_NM": "Back in Form"
+            "XS_COURSE_TOPIC_NM": "Form 1 here"
         }
     ],
     [
         {
             "COURSE_TOPIC_ID": "195",
             "FORM_TOPIC_ID": "21",
-            "XS_COURSE_TOPIC_NM": "Back in Form"
+            "XS_COURSE_TOPIC_NM": "Da other form here"
         },
         {
             "COURSE_TOPIC_ID": "122",
             "FORM_TOPIC_ID": "22",
-            "XS_COURSE_TOPIC_NM": "Behaviour Management and Mental Health"
+            "XS_COURSE_TOPIC_NM": "Mental Form Here"
         },
         {
             "COURSE_TOPIC_ID": "132",
             "FORM_TOPIC_ID": "23",
-            "XS_COURSE_TOPIC_NM": "Communications"
+            "XS_COURSE_TOPIC_NM": "Pepe form here"
         }
     ],
     [
         {
             "COURSE_TOPIC_ID": "122",
             "FORM_TOPIC_ID": "22",
-            "XS_COURSE_TOPIC_NM": "Behaviour Management and Mental Health"
+            "XS_COURSE_TOPIC_NM": "Madre mia here"
         },
         {
             "COURSE_TOPIC_ID": "195",
